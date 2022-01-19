7de8cfffa9606240a84dadd2f2756dbec6610aa4
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-19 15:35:39 -0600

    UPDATE App.js: Change file boilerplate comment

--- js/App.js
+++ js/App.js
@@ -1,22 +1,21 @@
 /*
-Program Name:   EHW APP: Scrape LinkedIn Course Info
+Program Name:   EHW APP: Scrape Upwork Job Details
 File Name:      App.js
-Date Created:   01/17/22
+Date Created:   01/19/22
 Date Modified:  --
 Version:        1.00
 Programmer:     Eric L. Hepperle
 
-TAGS:           Eric Hepperle, JavaScript, ES6, DOM Manipulation, Demo, Eric L. Hepperle
+TAGS:           Eric Hepperle, JavaScript, ES6, DOM Manipulation, App, Eric L. Hepperle
 
 Purpose: 
     This JavaScript file contains the main "business logic" and primary
-    mechanics of the current project. This project scrapes 
-    a selection of useful course info from a LinkedIn Learning
-    course page.
+    mechanics of the current project. This project scrapes job details
+    from Upwork job posts.
     
 Usage:
-    Navigate to any video of a LinkedIn Learning course and paste
-    this code into the browser developer console and hit enter to run.
+    Navigate to any Upwork job post page and paste this code into the
+    browser developer console and hit enter to run.
 
 Sample results: 
 
