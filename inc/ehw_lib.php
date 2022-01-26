b76f4f0c16ac9e83391a4487ef2f66ce191077e8
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-26 15:18:24 -0600

    UPDATE ehw_lib.php: Add get_urls func

--- inc/ehw_lib.php
+++ inc/ehw_lib.php
@@ -17,6 +17,9 @@ function get_web_content($search_url) {
 
 /**
  * Get and return array of image urls from web content
+ * 
+ * Sample Regex:
+ * '!https://media-exp1.licdn.com/dms/image/[^\s"]*!';
  */
 function get_img_urls($html, $reg) {
     // Find and return array of all image urls
@@ -25,4 +28,16 @@ function get_img_urls($html, $reg) {
     // Get unique urls only
     $images = array_values(array_unique($matches[0]));
     return $images;
+}
+
+/**
+ * Get and return array of urls from web content
+ */
+function get_urls($html, $reg) {
+    // Find and return array of all image urls
+    preg_match_all($reg, $html, $matches);
+
+    // Get unique urls only
+    $urls = array_values(array_unique($matches[0]));
+    return $urls;
 }
