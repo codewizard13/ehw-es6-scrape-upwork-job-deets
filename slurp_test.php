cb7e4054a61f0001371c1e7b6a41d58708157552
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-26 11:16:48 -0600

    UPDATE slurp_test.php: fix commented URLS

--- slurp_test.php
+++ slurp_test.php
@@ -17,16 +17,16 @@ $search_url = $base_url . '/search?keywords=' . rawurlencode($search_str);
 
 /*
 https://media-exp1.licdn.com/dms/image/C4E0DAQF2DX4_GzNtiw/learning-public-crop_144_256/0/1567115269108?e=1642798800&v=beta&t=-Z1yy9gsSMO2fwexPwwbroo1kausegBWWVpyGZAVJOY
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4D0DAQG6dQ0gh0wiAw/learning-public-crop_144_256/0/1622221555885?e=1642798800&v=beta&t=O0V5Si-232Czwg3Xjylej-0T09p9dSqM-HoGt46FCHw
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4E0DAQHGEmHS49qQxw/learning-public-crop_144_256/0/1591038530598?e=1642798800&v=beta&t=H0kpiE_OU64SMNgzg3nXcUOWz3fTdoQUFsST9oyIVJo
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4E0DAQEL8HR59bH8XQ/learning-public-crop_144_256/0/1567117874723?e=1642798800&v=beta&t=jU2HgEBTuw27DUNsj9e1ugRM6vSRnmCU0WFElSja_Cg
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4D0DAQHK-x5Xftt6NQ/learning-public-crop_144_256/0/1587058478275?e=1642798800&v=beta&t=i12RIkJBtJD-oy01WOeuXlohukFsn-YSgToNXsoGOL4
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4E0DAQHrK3_TAA5SMg/learning-public-crop_144_256/0/1595872229643?e=1642798800&v=beta&t=iGcJAIqQCANGJGT1xbfmHuwIzsv_LyBDhFUNj8pai9E
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4E0DAQHVilhWY2KSFw/learning-public-crop_144_256/0/1568059277687?e=1642798800&v=beta&t=y_M2adH5L2mPVEhftT5Q-I6X1u3oKNTLBbEIGJU66Y0
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4E0DAQHZ5J3EiCOgAw/learning-public-crop_144_256/0/1600448853491?e=1642798800&v=beta&t=czGVO4QCP2z2n8hhZA32mxGy3MbwkGP-3apUhu0SxyU
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4E0DAQG0jc7cAzGK8g/learning-public-crop_144_256/0/1567117555407?e=1642798800&v=beta&t=_tDzVfrkCEK7Fl17K5BM7YnDct_EN89p7j5kWgEFsLA
-VM1111:4 https://media-exp1.licdn.com/dms/image/C560DAQGv2jZ7bIgGcw/learning-public-crop_144_256/0/1610728168219?e=1642798800&v=beta&t=s3D9SUqPmCr0XC3cCK7qSmPQHlzlfTQsIWC5Imf34x4
-VM1111:4 https://media-exp1.licdn.com/dms/image/C4E0DAQFnjLm3IKbs6g/learning-public-crop_144_256/0/1573495097906?e=1642798800&v=beta&t=lIBxhpr5PGfD4GrdWQykrediv9e1X_8k0rtFUetNmm8
+https://media-exp1.licdn.com/dms/image/C4D0DAQG6dQ0gh0wiAw/learning-public-crop_144_256/0/1622221555885?e=1642798800&v=beta&t=O0V5Si-232Czwg3Xjylej-0T09p9dSqM-HoGt46FCHw
+https://media-exp1.licdn.com/dms/image/C4E0DAQHGEmHS49qQxw/learning-public-crop_144_256/0/1591038530598?e=1642798800&v=beta&t=H0kpiE_OU64SMNgzg3nXcUOWz3fTdoQUFsST9oyIVJo
+https://media-exp1.licdn.com/dms/image/C4E0DAQEL8HR59bH8XQ/learning-public-crop_144_256/0/1567117874723?e=1642798800&v=beta&t=jU2HgEBTuw27DUNsj9e1ugRM6vSRnmCU0WFElSja_Cg
+https://media-exp1.licdn.com/dms/image/C4D0DAQHK-x5Xftt6NQ/learning-public-crop_144_256/0/1587058478275?e=1642798800&v=beta&t=i12RIkJBtJD-oy01WOeuXlohukFsn-YSgToNXsoGOL4
+https://media-exp1.licdn.com/dms/image/C4E0DAQHrK3_TAA5SMg/learning-public-crop_144_256/0/1595872229643?e=1642798800&v=beta&t=iGcJAIqQCANGJGT1xbfmHuwIzsv_LyBDhFUNj8pai9E
+https://media-exp1.licdn.com/dms/image/C4E0DAQHVilhWY2KSFw/learning-public-crop_144_256/0/1568059277687?e=1642798800&v=beta&t=y_M2adH5L2mPVEhftT5Q-I6X1u3oKNTLBbEIGJU66Y0
+https://media-exp1.licdn.com/dms/image/C4E0DAQHZ5J3EiCOgAw/learning-public-crop_144_256/0/1600448853491?e=1642798800&v=beta&t=czGVO4QCP2z2n8hhZA32mxGy3MbwkGP-3apUhu0SxyU
+https://media-exp1.licdn.com/dms/image/C4E0DAQG0jc7cAzGK8g/learning-public-crop_144_256/0/1567117555407?e=1642798800&v=beta&t=_tDzVfrkCEK7Fl17K5BM7YnDct_EN89p7j5kWgEFsLA
+https://media-exp1.licdn.com/dms/image/C560DAQGv2jZ7bIgGcw/learning-public-crop_144_256/0/1610728168219?e=1642798800&v=beta&t=s3D9SUqPmCr0XC3cCK7qSmPQHlzlfTQsIWC5Imf34x4
+https://media-exp1.licdn.com/dms/image/C4E0DAQFnjLm3IKbs6g/learning-public-crop_144_256/0/1573495097906?e=1642798800&v=beta&t=lIBxhpr5PGfD4GrdWQykrediv9e1X_8k0rtFUetNmm8
 
 */
 
