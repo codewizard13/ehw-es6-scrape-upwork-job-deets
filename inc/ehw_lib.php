5dceebc096f3575b4b88d837d38b2f20af853bd3
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-26 15:14:41 -0600

    UPDATE ehw_lib.php: Add functions
    - Adds get_web_content()
    - Adds get_img_urls()

---
+++ inc/ehw_lib.php
@@ -0,0 +1,28 @@
+<?php // ehw_lib.php
+
+/**
+ * Slurp web page content with cURL and return html
+ */
+
+function get_web_content($search_url) {
+    
+    // Slurp page code with cURL
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $search_url);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    return curl_exec($curl);
+}
+
+/**
+ * Get and return array of image urls from web content
+ */
+function get_img_urls($html, $reg) {
+    // Find and return array of all image urls
+    preg_match_all($reg, $html, $matches);
+
+    // Get unique urls only
+    $images = array_values(array_unique($matches[0]));
+    return $images;
+}
