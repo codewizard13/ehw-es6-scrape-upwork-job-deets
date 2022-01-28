8d5a491bf7b56f53227bbf3d1a2b273491e30c88
Eric L. Hepperle <codewizard13@gmail.com>
2022-01-28 10:29:12 -0600

    ADD docs dir and ehw.prj.info.js (Project info)

---
+++ docs/ehw.prj.info.js
@@ -0,0 +1,51 @@
+/*
+This is the main details page for the project
+*/
+
+/*
+Project Name:   EHW APP: Scrape Upwork Job Details
+Proj Shortname: ES6: Scrape Upwork Job Deets 
+Main Prj File:  controller.php
+This Filename:  ehw.prj.info.js
+Date Created:   01/28/22
+Date Updated:   --
+Programmer:     Eric L. Hepperle
+
+Prj Purpose: 
+JavaScript ES6 web scraper to quickly grab job details
+from Upwork job posts. This will be used to build
+a markdown file to document jobs applied for.
+
+File Purpose:
+Holds project details used in header boilerplate comments.
+
+Usage:
+Navigate to any video of an Upwork job posting page and paste the
+code from App.js into the browser developer console and hit enter to run.
+
+Requires:
+* Browser navigated to an Upwork job posting page page
+* Web server supporting PHP 7.4
+* The project folder must live on a web server, local or remote.
+* Be logged into LinkedIn Learning
+
+Demonstrates:
+* ES6 Arrow Functions
+* Map, Reduce, Filter, Find
+* Promises, Closures, Anonymous Functions
+* SetTimeout, Timers,
+* Const, Let, Var
+* For-in, For-of, ForEach
+* Continue, Break, Switch-Case
+
+Tags:
+ES6, arrow functions, PHP variables
+
+Future:
+* Use curl or fetch API to grab course page and parse info from there
+
+*/
+
+/*
+Doc template by Eric L. Hepperle (c) 2022
+*/
